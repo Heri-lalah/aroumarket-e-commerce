330ff584d3c6d1be957c3c6270c4d91576f2efd5
Herilala <herilalah.rakotonasolo@gmail.com>
2022-07-05 16:28:34 -0400

    header beta finished

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
-    <title>Welcome</title>
+    <title>ArouMarket</title>
     <link rel="shortcut icon" href="{{ asset('avatars/logo.png') }}" type="image/x-icon">
 </head>
 <body>
