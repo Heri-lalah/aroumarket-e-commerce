aa982ff6e81fef565ee89ed884121a8a3d758a04
Herilala <herilalah.rakotonasolo@gmail.com>
2022-07-06 18:38:49 -0400

    Carousel in tmp- Initialize a layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,53 +1,14 @@
 @extends('layouts.template')
 
 @section('main')
-
-
-{{--CARROUSSEL SECTION--}}
-
-
-<div class="myCarousel">
-    <div class="carousel slide" data-bs-ride="carousel" id="productCarousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="{{ asset('images/coding.jpg') }}" alt="coding" class="d-block w-100 img-fluid">
-            </div>
-            <div class="carousel-item">
-                <img src="{{ asset('images/mixing.jpg') }}" alt="mixing" class="d-block w-auto img-fluid">
-            </div>
-            <div class="carousel-item">
-                <img src="{{ asset('images/teaching.jpg') }}" alt="teaching" class="d-block w-auto img-fluid">
-            </div>
+<div class="layout position-relative">
+    <section class="waves">
+        <div class="custom-shape-divider-top-1657143740">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path fill="#c7358a" d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+            </svg>
         </div>
-        <button type="button" class="carousel-control-prev" data-bs-slide="prev" data-bs-target="#productCarousel">
-            <span class="carousel-control-prev-icon"></span>
-        </button>
-        <button type="button" class="carousel-control-next" data-bs-slide="next" data-bs-target="#productCarousel">
-            <span class="carousel-control-next-icon"></span>
-        </button>
-    </div>
-</div>
-
-
-<div class="container-md mt-5">
-    <div class="row">
-        <div class="col-sm-12 col-md-3 text-center">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <p>Simple</p>
-        </div>
-        <div class="col-sm-12 col-md-3 text-center">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <p>Sécurisé</p>
-        </div>
-        <div class="col-sm-12 col-md-3 text-center">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <p>12h</p>
-        </div>
-        <div class="col-sm-12 col-md-3 text-center">
-            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-            <p>Service après vente</p>
-        </div>
-    </div>
+    </section>
 </div>
 
 @endsection
