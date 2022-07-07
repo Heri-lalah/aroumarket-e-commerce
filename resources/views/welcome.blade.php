9594a4b33a72344a18c194eda4cfa737dee15eb0
Herilala <herilalah.rakotonasolo@gmail.com>
2022-07-06 20:16:03 -0400

    Inserting text in banner unfinished

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.template')
 
 @section('main')
-<div class="layout position-relative">
-    <section class="overlay">
-        <div class="waves">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path fill="#c7358a" fill-opacity="1" d="M0,128L60,122.7C120,117,240,107,360,117.3C480,128,600,160,720,170.7C840,181,960,171,1080,149.3C1200,128,1320,96,1380,80L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-            </svg>
-        </div>
-    </section>
+<div class="banner">
+    <div class="layout position-relative">
+        <section class="overlay">
+            <div class="waves">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                  <path fill="#c7358a" fill-opacity="1" d="M0,128L60,122.7C120,117,240,107,360,117.3C480,128,600,160,720,170.7C840,181,960,171,1080,149.3C1200,128,1320,96,1380,80L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+                </svg>
+            </div>
+        </section>
+    </div>
+    <div class="container">
+        <p class="lorem">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias reprehenderit repudiandae eveniet asperiores temporibus ratione saepe tempore esse iste suscipit quis porro, dignissimos quo, quibusdam ipsa repellendus voluptas eum optio.</p>
+    </div>
 </div>
-
 @endsection
