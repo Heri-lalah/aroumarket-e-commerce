ad0b23a58f8113b9d553b9d364fe1f935068876b
Herilala <herilalah.rakotonasolo@gmail.com>
2022-07-07 20:36:43 -0400

    structiring Carousel in a footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,11 +21,24 @@
             </div>
         </div>
 
-        <div class="myCarousel">
-            <div class="carousel slide" data-bs-ride="carousel" id="productCarousel">
+        <div class="myCarousel mt-5 container">
+            <div class="carousel bg-primary slide" data-bs-ride="carousel" id="productCarousel">
                 <div class="carousel-inner">
-                    <div class="carousel-item"></div>
-                    <div class="carousel-item"></div>
+                    <div class="carousel-item active">
+                        <img src="{{ asset('images/coding.jpg') }}" alt="coding" class="d-block w-100 img-fluid">
+                        <div class="carousel-caption active">
+                            <h1>One more for good measure.</h1>
+                            <p>Some representative placeholder content for the third slide of this carousel.</p>
+                            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="carousel-caption">
+                            <h1>One more for good measure.</h1>
+                            <p>Some representative placeholder content for the third slide of this carousel.</p>
+                            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                        </div>
+                    </div>
                 </div>
                 <button type="button" class="carousel-control-prev mt-5" data-bs-slide="prev" data-bs-target="#productCarousel">
                     <span class="carousel-control-prev-icon"></span>
