c41fa330e7f3ea74d4f8767c9c5f22aa7aadf9b4
Herilala <herilalah.rakotonasolo@gmail.com>
2022-07-09 00:39:58 -0400

    Responsive content : ok, gradient non ok,Adding carousel slide an

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,3 @@
 <div class="footer bg-light fixed-bottom text-center">
-    <span class="footer-items">
-        <span class="brandsintro">Nous suivre :</span>
-        <a href="" class="d-inline-block m-1"><i class="fab fa-facebook"></i></a>
-        <a href="#" class="d-inline-block m-1"><i class="fab fa-twitter"></i></a>
-        <a href="#" class="d-inline-block m-1"><i class="fab fa-skype"></i></a>
-        <a href="" class="d-inline-block m-1"><i class="fab fa-google-plus-g"></i></a>
-    </span>
-    <p>&COPY; <span id="copy"></span></p>
+    <p>&COPY; <span id="footertime"> 2022</span></p>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="layout">
     <div class="col-12">
         <div class="wrapper">
-            <div class="intro mt-4 container">
+            <div class="intro mt-4">
                 <div class="d-lg-flex justify-content-between flex-lg-row-reverse">
                     <div class="text-center col-xl-4">
                         <img src="{{asset('avatars/logo.png')}}" alt="intro-image" width="150px">
@@ -21,33 +21,58 @@
     </div>
 
     <div class="services col-12 mt-lg-5 text-center container">
-       <div class="row">
-        <div class="col-sm-12 col-xl-8 align-self-center">
-            <div class="row">
-                <div class="col-sm-12 col-md-4">
-                    <i class="fa fa-money-bill-wave rounded-circle"></i>
-                    <p>Payement en toute sécurité</p>
+        <div class="row">
+            <div class="col-sm-12 col-xl-8">
+                <div class="d-lg-flex flex-lg-column">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-4">
+                            <i class="fa fa-money-bill-wave rounded-circle service-item"></i>
+                            <p class="serviceCaption">Payement en toute sécurité</p>
+                        </div>
+                        <div class="col-sm-12 col-md-4">
+                            <i class="fa fa-shipping-fast rounded-circle service-item"></i>
+                            <p class="serviceCaption">Livraison dans<span class="hours">4</span>heures</p>
+                        </div>
+                        <div class="col-sm-12 col-md-4">
+                            <i class="fas fa-tools rounded-circle service-item"></i>
+                            <p class="serviceCaption">Services après vente</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-sm-12 col-md-4">
-                    <i class="fa fa-shipping-fast rounded-circle"></i>
-                    <p>Livraison dans <span>4</span>heures</p>
+
+                <div class="info container ">
+                    <p class="col-sm-12 col-xl-auto mt-md-4">
+                        N'hesitez pas à nous appeler pour toute information complémentaire
+                    </p>
+                    <p class="col-sm-12 col-xl-auto">
+                        <span class="brands text-center">
+                            <a href="#" class="d-inline-block m-1"><i class="fab fa-facebook"></i></a>
+                            <a href="#" class="d-inline-block m-1"><i class="fab fa-twitter"></i></a>
+                            <a href="#" class="d-inline-block m-1"><i class="fab fa-skype"></i></a>
+                            <a href="" class="d-inline-block m-1"><i class="fab fa-google-plus-g"></i></a>
+                        </span>
+                    </p>
                 </div>
-                <div class="col-sm-12 col-md-4">
-                    <i class="fas fa-tools rounded-circle"></i>
-                    <p>Services après vente</p>
+
+            </div>
+
+            {{-- IMAGES PUB --}}
+            <div class="d-none d-xl-block col-xl-4 align-self-center8 position-relative">
+                <div class="carousel slide" data-bs-ride="carousel" id="productCarousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="{{ asset('images/coding.jpg') }}" alt="coding" class="d-block w-100 img-fluid">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{ asset('images/mixing.jpg') }}" alt="mixing" class="d-block w-auto img-fluid">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{ asset('images/teaching.jpg') }}" alt="teaching" class="d-block w-auto img-fluid">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="d-none d-xl-block col-xl-4">
-            <img src="{{asset('images/coding.jpg')}}" alt="" class="img-fluid">
-        </div>
-       </div>
-    </div>
-    <div class="info container">
-        <p class="col-sm-12 col-xl-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio debitis optio eveniet mollitia aliquid ipsam, omnis, sit id sunt eum harum assumenda repellat corrupti, ex vero? Dolor neque dolores impedit!
-        </p>
-        <p class="d-none col col-xl-6 d-md-block"></p>
     </div>
 </div>
 @stop
