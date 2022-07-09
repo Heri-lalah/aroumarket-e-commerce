f0d78abb0b1e5bfc5e35b2864873f45af54ab653
Herilala <herilalah.rakotonasolo@gmail.com>
2022-07-09 17:52:06 -0400

    Inserting fixed categorie br

--- resources/views/Pagination/products/products.blade.php
+++ resources/views/Pagination/products/products.blade.php
@@ -1,136 +1,30 @@
 @extends('layouts.template')
 @section('main')
 <div class="banner">
-    <div class="productslist container">
-        <div class="row g-4">
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="" class="img-fluid">
-                </div>
+    <div class="row m-2">
+        <div class="col-sm-2 position-sticky">
+            <div class="navbar-nav flex-column">
+                <p>Catégories</p>
+                <ul class="nav flex-column">
+                    <li class="nav-item bg-info"><a href="" class="nav-link">Item1</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Item1</a></li>
+                    <li class="nav-item"><a href="" class="nav-link">Item1</a></li>
+                </ul>
             </div>
-
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="" class="img-fluid">
-                </div>
-            </div>
-
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="" class="img-fluid">
-                </div>
-            </div>
-
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="" class="img-fluid">
-                </div>
-            </div>
-
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="" class="img-fluid">
-                </div>
-            </div>
-
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="card">
-                    <img src="{{asset('images/coding.jpg')}}" alt="">
+        </div>
+        <div class="col-sm-10">
+            <div class="productslist">
+                <div class="row g-4">
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card position-relative">
+                            <img src="{{asset('images/coding.jpg')}}" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card position-relative">
+                            <img src="{{asset('images/coding.jpg')}}" alt="" class="img-fluid">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
