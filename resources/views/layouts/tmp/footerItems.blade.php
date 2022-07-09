2ab2d49d1489f8995c02abb7f9e34e00f9f56f60
Herilala <herilalah.rakotonasolo@gmail.com>
2022-07-09 16:44:56 -0400

    public css modified

---
+++ resources/views/layouts/tmp/footerItems.blade.php
@@ -0,0 +1,7 @@
+<span class="footer-items">
+    <span class="brandsintro">Nous suivre :</span>
+    <a href="" class="d-inline-block m-1"><i class="fab fa-facebook"></i></a>
+    <a href="#" class="d-inline-block m-1"><i class="fab fa-twitter"></i></a>
+    <a href="#" class="d-inline-block m-1"><i class="fab fa-skype"></i></a>
+    <a href="" class="d-inline-block m-1"><i class="fab fa-google-plus-g"></i></a>
+</span>
