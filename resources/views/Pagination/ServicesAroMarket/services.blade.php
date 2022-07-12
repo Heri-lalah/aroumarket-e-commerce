26ebda2d7410fa896c4ddc30f8766210e3291c4a
Herilala <herilalah.rakotonasolo@gmail.com>
2022-07-12 04:51:37 -0400

    Adding firt content

--- resources/views/Pagination/ServicesAroMarket/services.blade.php
+++ resources/views/Pagination/ServicesAroMarket/services.blade.php
@@ -2,7 +2,12 @@
 @section('main')
 <div class="bannerService">
     <div class="container">
-        <p>Je suis le premier text dans la page "services"</p>
+        <div class="intro text-center">
+            <p><img src="{{asset('avatars/logo.png')}}" alt="logo" width="100px"></p>
+            <p>ArouMarket est l'un de système performant de la vente en ligne(bientôt boutique) qui necessite moins de temps qu'il faudra.
+                Il suffit juste de commander et payer même vous n'est pas encore membre de la groupe.
+            </p>
+        </div>
     </div>
 </div>
 @endsection
