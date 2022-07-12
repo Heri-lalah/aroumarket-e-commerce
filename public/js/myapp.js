7df57d23027367e3d25d20e556b5e571af0af6e3
Herilala <herilalah.rakotonasolo@gmail.com>
2022-07-12 04:23:55 -0400

    public css/js modified/Adding myapp js

---
+++ public/js/myapp.js
@@ -0,0 +1,42 @@
+//Global animation
+let tohome=document.getElementById('tohome');
+document.addEventListener('scroll', function () {
+    if(window.scrollY>80){
+        tohome.classList.replace('tohome','tohomeactive')
+    }else{
+        tohome.classList.replace('tohomeactive','tohome')
+    }
+})
+
+
+//Cart animation
+let cards=document.querySelectorAll('.card');
+for(let i=0; i<cards.length;i++){
+    let card=cards[i];
+    card.addEventListener('mouseover', function () {
+        let fade=this.children[1];
+        let productinfo=this.children[2]
+        let addto=this.children[3];
+        fade.classList.add('fadeactive');
+        addto.classList.add('activecard');
+        productinfo.classList.add('activeproductinfo');
+    })
+    card.addEventListener('mouseout', function () {
+        let fade=this.children[1];
+        let productinfo=this.children[2]
+        let addto=this.children[3];
+        let btnaddinfo=this.children[4];
+        fade.classList.remove('fadeactive');
+        addto.classList.remove('activecard');
+        productinfo.classList.remove('activeproductinfo');
+    })
+
+    let addto=card.children[3];
+    let btnaddinfo=card.children[4];
+    addto.addEventListener('mouseover', function () {
+        btnaddinfo.classList.add('addinfoactive');
+    })
+    addto.addEventListener('mouseout', function () {
+        btnaddinfo.classList.remove('addinfoactive');
+    })
+}
