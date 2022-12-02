dff08e948a683ede726070774e7f776ae96c1f24
Herilala <herilalah.rakotonasolo@gmail.com>
2022-12-02 09:37:10 +0300

    Ajout model Order avec migration et controller

---
+++ database/migrations/2022_12_02_060106_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_number');
+            $table->integer('total_price');
+            $table->dateTime('shipped_at')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+
+        Schema::create('order_product', function (Blueprint $table){
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
